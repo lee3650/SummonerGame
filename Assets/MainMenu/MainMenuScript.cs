5dd67a4f756fdd9ee5d5d5e89e93e00ceef52b39
Isaac Lee <lee3650@purdue.edu>
2021-10-19 10:46:51 -0400

    Added main menu scene and script

---
+++ Assets/MainMenu/MainMenuScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    private static bool TutorialMode = true; //we'll have to load this from a file, or use PlayerPrefs. 
+    private string tutorialModeKey = "TutorialMode";
+
+    private void Awake()
+    {
+        TutorialMode = PlayerPrefs.GetInt(tutorialModeKey, 1) == 1 ? true : false;
+
+        ShowPlayButtons();
+    }
+
+    public void PlayButtonPressed()
+    {
+
+    }
+
+    private void ShowPlayButtons()
+    {
+
+    }
+}
