0b157088dc4be49d83ca5623e19b76bcf5d2126e
Isaac <lee3650@purdue.edu>
2021-11-24 12:09:20 -0600

    Modified progression menu - setup UI

---
+++ Assets/UI/MouseoverGrey.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class MouseoverGrey : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [Tooltip("If null, assigned at awake to GetComponent")]
+    [SerializeField] Image image;
+
+    private void Awake()
+    {
+        if (image == null)
+        {
+            image = GetComponent<Image>();
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData data)
+    {
+        image.color = new Color(0.75f, 0.75f, 0.75f, 1);
+    }
+
+    public void OnPointerExit(PointerEventData data)
+    {
+        image.color = new Color(1f, 1f, 1f, 1);
+    }
+}
